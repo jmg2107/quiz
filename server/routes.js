f3d04f5b40487fcbaddd8b884439e2f7bc5e2858
jmg2107 <jenjengoo@gmail.com>
2016-09-01 17:33:10 -0700

    (feat) Setup initial infrastructure for project

---
+++ server/routes.js
@@ -0,0 +1,4 @@
+module.exports = function (app, express){
+  //setup routes
+
+};
---
+++ server/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+
+require('./middleware.js')(app, express);
+require('./routes.js')(app, express);
+
+module.exports = app;
